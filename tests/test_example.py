9d98f4efd572cb24a7f01ba5ad5243d858aaf689
Ilya <northenlight6899@gmail.com>
2025-10-07 23:33:30 +0300

    init commit

---
+++ tests/test_example.py
@@ -0,0 +1,15 @@
+import pytest
+from playwright.sync_api import Page, expect
+
+def test_health(page: Page):
+    assert 1
+
+def test_has_title(page: Page):
+    page.goto("https://www.saucedemo.com/")
+    expect(page).to_have_title("Swag Labs")
+
+@pytest.mark.skip(reason="Wrong test")
+def test_has_login_button(page: Page):
+    page.goto("https://www.saucedemo.com/")
+    expect(page.get_by_role("submit")).to_be_visible()
+
