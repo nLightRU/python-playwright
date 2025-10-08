8a931fe37cab052f14abb2de7212451596eaaafa
Ilya <northenlight6899@gmail.com>
2025-10-08 21:35:54 +0300

    fix test by test id and add requrements.txt

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,5 @@
 import pytest
-import playwright
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Playwright, Page, expect
 
 # To Do: Check placeholder
 
@@ -12,10 +11,11 @@ def test_success_login(page: Page):
     page.locator('id=password').fill('secret_sauce')
     page.locator('id=login-button').click()
     page.wait_for_timeout(5000.0)
+    expect(page.locator('.inventory_list')).to_be_visible()
 
 
-@pytest.mark.skip
-def test_success_login_by_test_id(page: Page):
+@pytest.mark.login
+def test_success_login_by_test_id(playwright: Playwright, page: Page):
     playwright.selectors.set_test_id_attribute('data-test')
     page.goto('https://www.saucedemo.com/')
     page.get_by_test_id('username').fill('standard_user')
@@ -29,3 +29,5 @@ def test_empty_fields(page: Page):
     page.goto('https://www.saucedemo.com/')
     page.locator('id=login-button').click()
     page.wait_for_timeout(5000.0)
+    expect(page.locator('.error-message-container')).to_be_visible()
+    
