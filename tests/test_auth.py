8ff6b7b7b61fa6bf22a07a7149dec868d64474f0
Ilya <northenlight6899@gmail.com>
2025-12-13 00:21:57 +0300

    added a few auth tests and edited some existed

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -2,54 +2,92 @@ import pytest
 from playwright.sync_api import Playwright, Page, expect
 
 # To Do: Check placeholder
+# To Do: Вынести сообщения об ошибках отдельно
 
-# test with location by id
-# @pytest.mark.smoke
-# @pytest.mark.login
-# def test_success_login(page: Page):
-#     page.goto('https://www.saucedemo.com/')
-#     page.locator('id=user-name').fill('standard_user')
-#     page.locator('id=password').fill('secret_sauce')
-#     page.locator('id=login-button').click()
-#     page.wait_for_timeout(5000.0)
-#     expect(page.locator('.inventory_list')).to_be_visible()
-
-
-@pytest.mark.login
+# Тест успешной авторизации в сервисе
+@pytest.mark.auth
 def test_success_auth(playwright: Playwright, page: Page):
     playwright.selectors.set_test_id_attribute('data-test')
     page.goto('https://www.saucedemo.com/')
     page.get_by_test_id('username').fill('standard_user')
     page.get_by_test_id('password').fill('secret_sauce')
     page.get_by_test_id('login-button').click()
+    expect(page.get_by_test_id('title')).to_be_visible()
+    expect(page.get_by_test_id('title')).to_have_text('Products')
 
 
-@pytest.mark.login
+# Тест выхода из аккаунта
+# Здесь надо тестить сразу на залогиненом аккаунте!!!!
+@pytest.mark.auth
+@pytest.mark.skip(reason="Not implemented yet")
+def test_logout(playwright: Playwright, page: Page):
+    playwright.selectors.set_test_id_attribute('data-test')
+    page.goto('https://www.saucedemo.com/')
+
+
+@pytest.mark.auth
 def test_locked_out_user(playwright: Playwright, page: Page):
     playwright.selectors.set_test_id_attribute('data-test')
     page.goto('https://www.saucedemo.com/')
     page.get_by_test_id('username').fill('locked_out_user')
     page.get_by_test_id('password').fill('secret_sauce')
     page.get_by_test_id('login-button').click()
-    expect(page.locator('.error-message-container')).to_be_visible()
+    expect(page.get_by_test_id('error')).to_be_visible()
     expect(page.get_by_text('Epic sadface: Sorry, this user has been locked out.')).to_be_visible()
 
 
 # Появляется ошибка если поля пустые
-@pytest.mark.login
+@pytest.mark.auth
 @pytest.mark.negative
 def test_empty_fields(playwright: Playwright, page: Page):
     playwright.selectors.set_test_id_attribute('data-test')
     page.goto('https://www.saucedemo.com/')
     page.locator('id=login-button').click()
-    # page.wait_for_timeout(5000.0)
-    expect(page.locator('.error-message-container')).to_be_visible()
+    expect(page.get_by_test_id('error')).to_be_visible()
     expect(page.get_by_text('Epic sadface: Username is required')).to_be_visible()
 
 
+# Появляется ошибка если пароль неправильный
+@pytest.mark.auth
+@pytest.mark.negative
+def test_wrong_password(playwright: Playwright, page: Page):
+    playwright.selectors.set_test_id_attribute('data-test')
+    page.goto('https://www.saucedemo.com/')
+    page.get_by_test_id('username').fill('standard_user')
+    page.get_by_test_id('password').fill('1234666')
+    page.get_by_test_id('login-button').click()
+    expect(page.get_by_test_id('error')).to_be_visible()
+    expect(page.get_by_text('Epic sadface: Username and password do not match any user in this service')).to_be_visible()
+
+
+# Появляется ошибка если имя пользователя неправильное, а пароль правильный
+@pytest.mark.auth
+def test_wrong_username(playwright: Playwright, page: Page):
+    playwright.selectors.set_test_id_attribute('data-test')
+    page.goto('https://www.saucedemo.com/')
+    page.get_by_test_id('username').fill('qweasd')
+    page.get_by_test_id('password').fill('secret_sauce')
+    page.get_by_test_id('login-button').click()
+    expect(page.get_by_test_id('error')).to_be_visible()
+    expect(page.get_by_text('Epic sadface: Username and password do not match any user in this service')).to_be_visible()
+
+
+# Наличие плейсхолдеров и их правильное значение
+@pytest.mark.UI
+def test_placeholders(playwright: Playwright, page: Page):
+    playwright.selectors.set_test_id_attribute('data-test')
+    page.goto('https://www.saucedemo.com/')
+    expect(page.get_by_test_id('username')).to_have_attribute('placeholder', 'Username')
+    expect(page.get_by_test_id('password')).to_have_attribute('placeholder', 'Password')
+
+
 # После неправильной авторизации крестиком можно закрыть ошибку
+# Пока не знаю как проверять CSS свойства элементов
 @pytest.mark.UI
 def test_error_close(playwright: Playwright, page: Page):
     playwright.selectors.set_test_id_attribute('data-test')
     page.goto('https://www.saucedemo.com/')
-    pass
+    page.get_by_test_id("login-button").click()
+    expect(page.get_by_test_id('error')).to_be_visible()
+    page.get_by_test_id('error-button').click()
+    expect(page.get_by_test_id('error')).not_to_be_visible()
