516365cc753d06cdf0e555e7c3a7f3b9179f5c25
Ilya <northenlight6899@gmail.com>
2026-02-09 18:02:43 +0300

    some work

---
+++ main.py
@@ -0,0 +1,38 @@
+import time
+from typing import List
+from playwright.sync_api import sync_playwright, Page, Locator
+
+url = 'https://www.saucedemo.com/inventory.html'
+
+
+def login(page: Page):
+    page.locator('#user-name').fill('standard_user')
+    page.locator('#password').fill('secret_sauce')
+    page.get_by_role('button').click()
+
+
+def get_inventory_list(page: Page) -> List[Locator]:
+    items = page.get_by_test_id('inventory-item').all()
+    return items
+
+
+def cart_badge_count(page: Page) -> int:
+    badge = page.get_by_test_id('shopping-cart-badge')
+    return int(badge.text_content())
+    
+
+if __name__ == '__main__':
+    with sync_playwright() as p:
+        p.selectors.set_test_id_attribute('data-test')
+        browser = p.chromium.launch(channel='chrome', headless=False)
+        page = browser.new_page()
+        page.goto('https://www.saucedemo.com')
+        login(page)
+        items = get_inventory_list(page)
+        items[0].get_by_role('button').click()
+        
+        # print(cart_badge_count(page))
+
+        # assert cart_badge_count(page) == 1
+
+        time.sleep(5)
